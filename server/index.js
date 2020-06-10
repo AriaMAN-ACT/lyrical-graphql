b23ec66d71830750726049fe33d1a292b625b6e9
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-10 17:13:55 +0430

    create server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const webpackMiddleware = require('webpack-dev-middleware');
+const bodyParser = require('body-parser');
+
+const schema = require('./schema');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql: true
+}));
+
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;
