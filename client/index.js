6f9e8e6300c453a3315c75c5fef72bb9b3cda1aa
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-10 21:44:16 +0430

    add react router dom and create app component

--- client/index.js
+++ client/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import {ApolloProvider} from 'react-apollo';
 
+import App from "./components/App";
 import SongList from "./components/SongList";
 
 const apolloClient = new ApolloClient({
@@ -15,7 +22,15 @@ const apolloClient = new ApolloClient({
 const Root = () => {
     return (
         <ApolloProvider client={apolloClient}>
-            <SongList/>
+            <Router>
+                <App>
+                    <Switch>
+                        <Route path="/">
+                            <SongList/>
+                        </Route>
+                    </Switch>
+                </App>
+            </Router>
         </ApolloProvider>
     );
 };
